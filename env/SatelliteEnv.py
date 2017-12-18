ecbe7f85a405124a17581dd9b3f3a622596d8729
hzc <huangzc13@mails.tsinghua.edu.cn>
2017-12-18 22:52:26 +0800

    Init gitigonre and gym env

---
+++ env/SatelliteEnv.py
@@ -0,0 +1,18 @@
+import gym
+
+class SatelliteEnv(gym.Env):
+
+    def __init__(self):
+        pass
+
+    def _step(self, action):
+        pass
+
+    def _seed(self, seed):
+        pass
+
+    def _reswt(self):
+        pass
+
+    def _render(self, mode='human', close=False):
+        pass
