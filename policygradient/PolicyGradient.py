70b14e622e0835a6dd49dbf3fcfdb026d30fbaee
hzc <huangzc13@mails.tsinghua.edu.cn>
2018-01-01 14:55:35 +0800

    Gradient is update once per iteration

--- policygradient/PolicyGradient.py
+++ policygradient/PolicyGradient.py
@@ -5,9 +5,10 @@ import numpy as np
 class PolicyGradient:
 
     def __init__(self, hidden_unit=[200,200], input_dim=7, output_dim=3, init_std = 1e-3, gamma=0.99):
-        self.input = tf.placeholder(dtype=tf.float32, shape=[None, input_dim])
-        self.target = tf.placeholder(dtype=tf.float32, shape=[None])
-        self.action = tf.placeholder(dtype=tf.float32, shape=[None, output_dim])
+        amp = 1e-4
+        self.input = tf.placeholder(dtype=tf.float32, shape=[None, None, input_dim])
+        self.target = tf.placeholder(dtype=tf.float32, shape=[None, None])
+        self.action = tf.placeholder(dtype=tf.float32, shape=[None, None, output_dim])/amp;
         self.lr_rate = tf.placeholder(dtype=tf.float32)
         self.gamma=gamma
 
@@ -17,15 +18,17 @@ class PolicyGradient:
 
         mean = tl.layers.DenseLayer(network, n_units=output_dim, W_init=tf.truncated_normal_initializer(stddev=init_std), name="mean").outputs
         std = tl.layers.DenseLayer(network, n_units=output_dim, act=tf.nn.softplus, W_init=tf.truncated_normal_initializer(stddev=init_std), name="std").outputs + 1e-5
+
         train_params = network.all_params
         normal_dist = tf.contrib.distributions.Normal(mean, std)
-        self.action_predict = tf.squeeze(normal_dist.sample([1]))
-        log_prob = tf.reduce_prod(tf.squeeze(normal_dist.log_prob(self.action)), axis=1)
-        self.loss = tf.reduce_mean(tf.multiply(-log_prob, self.target), axis=0)
+        self.action_predict = tf.squeeze(normal_dist.sample([1])) * amp
+        log_prob = tf.reduce_prod(tf.squeeze(normal_dist.log_prob(self.action)), axis=2)
+        self.loss = tf.reduce_sum(tf.reduce_mean(tf.multiply(-log_prob, self.target), axis=1), axis=0)
         self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr_rate).minimize(self.loss, var_list=train_params)
 
         self.action_list = list()
         self.state_list = list()
+        self.oplist = [mean, std]
 
     def _clearMem(self):
         self.action_list.clear()
@@ -40,20 +43,25 @@ class PolicyGradient:
         self._memorize(action, state)
         return action
 
+    def debug(self, state, sess):
+        return sess.run(self.oplist, {self.input: state})
+
     def update(self, reward, lr_rate, sess):
-        target = np.zeros(len(reward[0]), dtype=np.float32)
+        reward_array = np.array(reward)
+        target = np.zeros(reward_array.shape, dtype=np.float32)
+        temp = np.zeros(target.shape[1], dtype=np.float32)
         i = len(reward) - 1
         while i>0:
-            target = self.gamma*target + reward[i]
-            t = target - np.mean(target)
-            feed_dict = {
-                self.input: self.state_list[i],
-                self.action: self.action_list[i],
-                self.target: t,
-                self.lr_rate:lr_rate
-            }
-            _, loss = sess.run([self.train_op, self.loss], feed_dict=feed_dict)
-            i = i-1
+            temp  = self.gamma*temp + reward[i]
+            target[i] = temp - np.mean(temp)
+
+        feed_dict = {
+            self.input: np.array(self.state_list),
+            self.action: np.array(self.action_list),
+            self.target: target,
+            self.lr_rate:lr_rate
+        }
+        _, loss = sess.run([self.train_op, self.loss], feed_dict=feed_dict)
 
         self._clearMem()
         return target
